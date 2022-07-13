0162bd2365c94937159c1f1d758206b0a7784637
ColeJJ <torben.unland@gmail.com>
2022-07-13 10:51:59 +0200

    changed theme to vscode dark theme

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -25,32 +25,32 @@ o.si = true --Smart indent
 
 -- THEME VSCODE
 -- For dark theme (neovim's default)
---- vim.o.background = "dark"
+vim.o.background = "dark"
 -- Enable transparent background
---- vim.g.vscode_transparent = 1
+vim.g.vscode_transparent = 1
 -- Enable italic comment
---- vim.g.vscode_italic_comment = 1
+vim.g.vscode_italic_comment = 1
 -- Disable nvim-tree background color
---- vim.g.vscode_disable_nvimtree_bg = true
---- vim.cmd([[colorscheme vscode]])
+vim.g.vscode_disable_nvimtree_bg = true
+vim.cmd([[colorscheme vscode]])
 
--- THEME
+-- THEME Devaslife
 -- vim cmd run vim in lua
-vim.cmd([[
-" true color
-if exists("&termguicolors") && exists("&winblend")
-  syntax enable
-  set termguicolors
-  set winblend=0
-  set wildoptions=pum
-  set pumblend=5
-  set background=dark
-  " Use NeoSolarized
-  let g:neosolarized_termtrans=1
-  runtime ./colors/NeoSolarized.vim
-  colorscheme NeoSolarized
-endif
-]])
+--vim.cmd([[
+--" true color
+--if exists("&termguicolors") && exists("&winblend")
+--syntax enable
+--set termguicolors
+--set winblend=0
+--set wildoptions=pum
+--set pumblend=5
+--set background=dark
+--" Use NeoSolarized
+--let g:neosolarized_termtrans=1
+--runtime ./colors/NeoSolarized.vim
+--colorscheme NeoSolarized
+--endif
+--]])
 
 -- window-local options
 wo.number = false
