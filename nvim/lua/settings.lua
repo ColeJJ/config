269c275e225e60f730f69c60e057d21a83f3d266
ColeJJ <torben.unland@gmail.com>
2022-10-09 14:17:34 +0200

    commented vscode theme

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -23,14 +23,14 @@ o.si = true --Smart indent
 
 -- THEME VSCODE
 -- For dark theme (neovim's default)
-vim.o.background = "dark"
+-- vim.o.background = "dark"
 -- Enable transparent background
-vim.g.vscode_transparent = 1
+-- vim.g.vscode_transparent = 1
 -- Enable italic comment
-vim.g.vscode_italic_comment = 1
+-- vim.g.vscode_italic_comment = 1
 -- Disable nvim-tree background color
-vim.g.vscode_disable_nvimtree_bg = true
-vim.cmd([[colorscheme vscode]])
+-- vim.g.vscode_disable_nvimtree_bg = true
+-- vim.cmd([[colorscheme vscode]])
 
 -- THEME Devaslife
 -- vim cmd run vim in lua
