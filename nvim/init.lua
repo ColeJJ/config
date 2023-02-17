1c0d91ce9c8d1ea1e6f202b280349202ce01eb9a
ColeJJ <torben.unland@gmail.com>
2023-02-17 21:56:53 +0100

    added alacritty to the config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,34 @@
-require('plugins')
-require('settings')
-require('maps')
-require('lsp')
-require('cmp-config')
-require('treesitter')
-require('nvimtree-config')
-require('telescope-config')
-require('autopairs')
-require('ts-autotag')
-require('lspkind-config')
-require('lualine-config')
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
+require "core"
+require "core.options"
+
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
+end
+
+pcall(require, "custom")
+
+require("core.utils").load_mappings()
